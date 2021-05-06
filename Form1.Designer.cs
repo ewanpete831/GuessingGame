05a98665d59bc236afcef7737282567f8284d672
ewanpete831 <ewanpete831@outlook.com>
2021-05-06 11:45:43 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace GuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleLabel2 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.distanceLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(0, 1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(689, 97);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Guessing Game";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel2
+            // 
+            this.titleLabel2.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel2.Location = new System.Drawing.Point(173, 86);
+            this.titleLabel2.Name = "titleLabel2";
+            this.titleLabel2.Size = new System.Drawing.Size(355, 50);
+            this.titleLabel2.TabIndex = 1;
+            this.titleLabel2.Text = "Guess a number between 1 - 100";
+            this.titleLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(252, 139);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(171, 26);
+            this.inputBox.TabIndex = 2;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(297, 202);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(90, 63);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(90, 288);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(511, 50);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(297, 442);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(90, 63);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Play Again";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Visible = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // distanceLabel
+            // 
+            this.distanceLabel.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.distanceLabel.Location = new System.Drawing.Point(90, 355);
+            this.distanceLabel.Name = "distanceLabel";
+            this.distanceLabel.Size = new System.Drawing.Size(511, 50);
+            this.distanceLabel.TabIndex = 6;
+            this.distanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(692, 517);
+            this.Controls.Add(this.distanceLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.titleLabel2);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label titleLabel2;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label distanceLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        //set up random number and real value variable
+        Random randomNum = new Random();
+
+        int realValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //pick a random number
+            realValue = randomNum.Next(1, 101);
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            //set up variables for the guess, and distance
+            int guess;
+            int distance;
+            
+            try
+            {
+                //store the guess, and calculate the absolute distance from the real value
+                guess = Convert.ToInt32(inputBox.Text);
+                distance = realValue - guess;
+                int absoluteDistance = Math.Abs(distance);
+
+                //display message depending on if the player is above, below, or right on the real number
+                if (guess > realValue)
+                { 
+                    outputLabel.Text = "Too High!";
+                    inputBox.Clear();
+                }
+                else if (guess < realValue)
+                {
+                    outputLabel.Text = "Too Low!";
+                    inputBox.Clear();
+                }
+                else
+                { 
+                    outputLabel.Text = "You Win!";
+                    distanceLabel.Visible = false;
+                    guessButton.Enabled = false;
+                    //enable the reset button
+                    resetButton.Visible = true;
+                }
+
+                //display a message depending on how large the absolute distance is 
+                if (absoluteDistance > 50)
+                {
+                    distanceLabel.Text = "Freezing!";
+                }
+                else if (absoluteDistance > 25)
+                {
+                    distanceLabel.Text = "Cold";
+                }
+                else if (absoluteDistance > 15)
+                {
+                    distanceLabel.Text = "Cool";
+                }
+                else if (absoluteDistance > 10)
+                {
+                    distanceLabel.Text = "Warm";
+                }
+                else if(absoluteDistance >= 5)
+                {
+                    distanceLabel.Text = "Hot";
+                }
+                else
+                {
+                    distanceLabel.Text = "Boiling!";
+                }
+            }
+            catch
+            {
+                //display error message if invalid number is guessed
+                outputLabel.Text = "Please guess a Real Number!";
+                inputBox.Clear();
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            //pick a new random number, reset all values and text boxes
+            realValue = randomNum.Next(1, 101);
+            outputLabel.Text = null;
+            distanceLabel.Text = null;
+            distanceLabel.Visible = true;
+            guessButton.Enabled = true;
+            resetButton.Visible = false;
+            inputBox.Clear();
+        }
+    }
+}
